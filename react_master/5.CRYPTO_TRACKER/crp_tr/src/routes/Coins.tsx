4dddd4d3579a83ecfa6aa4afcc88293aef8306d3
ominyoung <ohmj090747@gmail.com>
2023-09-02 19:06:06 +0900

    REACT MASTER - typescript : interface 생성 - Router 작성 : Switch, Route, Link - 각 컴포넌트당 css 작성 방식 - useEffect 훅 사용 + async, await fetch - useRouteMatch로 해당 url 여부 확인
    2023-09-02

---
+++ react_master/5.CRYPTO_TRACKER/crp_tr/src/routes/Coins.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a{
+    display: flex;
+    align-content: center;
+    padding: 20px;
+    transition: color 0.2s ease-in-out;
+  }
+  &:hover{
+    a{
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const CoinWrapper = styled.span``;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`
+
+interface CoinInterface {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+}
+function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, [])
+    return (
+        <Container>
+            <Header>
+                <Title>Coins</Title>
+            </Header>
+            {loading ? <Loader>"Loading..."</Loader> : (<CoinsList>
+                {coins.map((coin) => (
+                    <Coin key={coin.id}>
+                        <Link to={{
+                            pathname: `/${coin.id}`,
+                            state:{ name: coin.name }
+                        }}>
+                            <CoinWrapper>
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt="logo" />
+                            {coin.name} &rarr;
+                            </CoinWrapper>
+                        </Link>
+                    </Coin>
+                ))}
+            </CoinsList>)}
+        </Container>
+    );
+}
+
+export default Coins;
